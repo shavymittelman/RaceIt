ef0845612441a8f518c0289f41c6f85dd5568136
shavy <shavymtl@gmail.com>
2023-07-18 13:22:44 -0400

    #3  code implementation

--- Race ItApp/Race ItApp/frmRaceIt.cs
+++ Race ItApp/Race ItApp/frmRaceIt.cs
@@ -15,6 +15,7 @@ namespace Race_ItApp
         Random rnd = new Random();
         List<Label> lstlblred;
         List<Label> lstlblblue;
+        enum SymbolEnum { A, G, H }
         enum TurnEnum { Red, Blue };
         TurnEnum currentturn = TurnEnum.Red;
 
@@ -24,9 +25,9 @@ namespace Race_ItApp
             btnStart.Click += BtnStart_Click;
             btnDie.Click += BtnDie_Click;
             NotPlaying();
-            lstlblred = new() 
+            lstlblred = new()
             {
-                lblRed1, lblRed2, lblRed3, lblRed4, lblRed5, lblRed6, lblRed7, lblRed8, lblRed9, lblRed10, 
+                lblRed1, lblRed2, lblRed3, lblRed4, lblRed5, lblRed6, lblRed7, lblRed8, lblRed9, lblRed10,
                 lblRed11, lblRed12, lblRed13, lblRed14, lblRed15, lblRed16, lblRed17, lblRed18, lblRed19, lblRed20
             };
             lstlblblue = new()
@@ -42,51 +43,80 @@ namespace Race_ItApp
             lblStatus.Text = "Click Start to begin game.";
         }
 
-        private void RandomSymbol(List<Label>list)
+        private void RandomSymbol(List<Label> list)
         {
             list.ForEach(lst => lst.Text = "");
+            //List<Label> lstrndlabel = new() {  }
             int randomlbl1 = rnd.Next(2, list.Count);
             int randomlbl2 = rnd.Next(2, list.Count);
-            int randomlbl3 = rnd.Next(2, list.Count);                       
+            int randomlbl3 = rnd.Next(2, list.Count);
+            int randomlbl4 = rnd.Next(2, list.Count);
+            int randomlbl5 = rnd.Next(2, list.Count);
+            int randomlbl6 = rnd.Next(2, list.Count);
             list.ForEach(lst => lst.Font = new Font("Wingdings", 20));
-            list[randomlbl1].Text = "G";
-            list[randomlbl2].Text = "A";
-            list[randomlbl3].Text = "H";     
+            list[randomlbl1].Text = SymbolEnum.A.ToString();
+            list[randomlbl2].Text = SymbolEnum.A.ToString();
+            list[randomlbl3].Text = SymbolEnum.G.ToString();
+            list[randomlbl4].Text = SymbolEnum.G.ToString();
+            list[randomlbl5].Text = SymbolEnum.H.ToString();
+            list[randomlbl6].Text = SymbolEnum.H.ToString();
         }
 
-        private void HitSymbol()
-        {
-            if (lstlblblue.ForEach(lst => lst.Text))
-        }
         private void StatusMessage(TurnEnum color)
         {
             lblStatus.Text = color + "'s turn to race!";
+        }
+        
+        private void HitSymbol(List<Label> labelstobefilled, List<Label> turnslist, Color turnscolor, Enum symbolenum, int takeamount)
+        {
+            if (labelstobefilled.LastOrDefault().Text == symbolenum.ToString())
+            {
+                turnslist.Where(lst => lst.BackColor.Equals(Color.Gainsboro)).Take(takeamount).ToList().ForEach(lst => lst.BackColor = turnscolor);
+            }
         }
 
         private void FillSquares(List<Label> list, Color turnscolor)
         {
             string advancestring = btnDie.Text;
             int.TryParse(advancestring, out int advancenumber);
-            List<Label> labelstobefilled = list.Where(b => b.BackColor.Equals(Color.Gainsboro)).Take(advancenumber).ToList();
+            List<Label> labelstobefilled = list.Where(lst => lst.BackColor.Equals(Color.Gainsboro)).Take(advancenumber).ToList();
             labelstobefilled.ForEach(lst => lst.BackColor = turnscolor);
+            HitSymbol(labelstobefilled, list, turnscolor, SymbolEnum.A, 2);
+            HitSymbol(labelstobefilled, list, turnscolor, SymbolEnum.G, 1);
+            if (labelstobefilled.LastOrDefault().Text == SymbolEnum.H.ToString())
+            {
+                list.Where(lst => lst.BackColor.Equals(turnscolor)).TakeLast(1).ToList().ForEach(lst => lst.BackColor = Color.Gainsboro);
+            }
         }
+
         private void SwitchTurn()
         {
             if (currentturn == TurnEnum.Red)
             {
                 FillSquares(lstlblred, Color.Red);
                 currentturn = TurnEnum.Blue;
-                StatusMessage(TurnEnum.Blue);                
+                StatusMessage(TurnEnum.Blue);
+                DetectWinner(lstlblred, Color.Red, TurnEnum.Red);
             }
             else
             {
                 FillSquares(lstlblblue, Color.Blue);
                 currentturn = TurnEnum.Red;
                 StatusMessage(TurnEnum.Red);
+                DetectWinner(lstlblblue, Color.Blue, TurnEnum.Blue);
             }
         }
-        
-        private void BtnStart_Click(object? sender, EventArgs e)
+
+        private void DetectWinner(List<Label> list, Color turnscolor, Enum color)
+        {
+            if (list.All(lst => lst.BackColor.Equals(turnscolor)))
+            {
+                lblStatus.Text = color + " Won!!!!!!!!!!";
+                btnDie.Enabled = false;
+            }
+        }
+
+        private void StartSetup()
         {
             btnDie.Enabled = true;
             lblStatus.Text = "Red's turn to race!";
@@ -97,12 +127,17 @@ namespace Race_ItApp
             RandomSymbol(lstlblred);
             RandomSymbol(lstlblblue);
             currentturn = TurnEnum.Red;
+            btnDie.Text = "Die";
+        }
+
+        private void BtnStart_Click(object? sender, EventArgs e)
+        {
+            StartSetup();
         }
 
         private void BtnDie_Click(object? sender, EventArgs e)
         {
             btnDie.Text = rnd.Next(1, 7).ToString();
-            
             SwitchTurn();
         }
     }
